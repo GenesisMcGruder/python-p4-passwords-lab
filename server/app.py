22fcb11f506d944db69b0d066b432d1391f789b7
Genesis McGruder <genesismcgruder@gmail.com>
2024-06-15 00:12:10 -0400

    done

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 
 from config import app, db, api
@@ -28,16 +28,41 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return jsonify(user.to_dict())
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        user = User.query.filter_by(username=username).first()
+
+        password = data.get('password')
+        
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            response = make_response(
+                jsonify(user.to_dict()),
+                200
+            )
+            return response
+            breakpoint()
+        return {'error':'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204, You are logged out'}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
